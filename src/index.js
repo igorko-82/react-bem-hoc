fca00f690524abc10ed08d0d1b8ccca0e6a540bf
Igor Khudo <igorko82@me.com>
2018-01-10 12:46:21 +0100

    formatting

--- src/index.js
+++ src/index.js
@@ -1,57 +1,57 @@
-function modifiersFromObj(baseClass, modifiers){
-  let modifiersClasses = [];
-  for(const modifier in modifiers){
-    modifiersClasses.push(createModifier(baseClass, modifier, modifiers[modifier]));
-  }
-  return modifiersClasses;
+function modifiersFromObj(baseClass, modifiers) {
+    let modifiersClasses = [];
+    for (const modifier in modifiers) {
+        modifiersClasses.push(createModifier(baseClass, modifier, modifiers[modifier]));
+    }
+    return modifiersClasses;
 }
 
-function createModifier(baseClass, modifierName, modifierValue){
-  if(!modifierValue || modifierValue === false) return '';
-  let className = `${baseClass}--${modifierName}`;
-  if(modifierValue && modifierValue !== true){
-    className += `-${modifierValue}`;
-  }
-  return className;
+function createModifier(baseClass, modifierName, modifierValue) {
+    if (!modifierValue || modifierValue === false) return '';
+    let className = `${baseClass}--${modifierName}`;
+    if (modifierValue && modifierValue !== true) {
+        className += `-${modifierValue}`;
+    }
+    return className;
 }
 
 module.exports = options => Component => {
 
-  const {block, modifiers} = options;
+    const {block, modifiers} = options;
 
-  Object.assign(Component.prototype, {
+    Object.assign(Component.prototype, {
 
-    block(passedmodifiers = {}){
-      let classesSet = [];
-      if(this.props.className){
-        classesSet.push(this.props.className);
-      }
+        block(passedmodifiers = {}) {
+            let classesSet = [];
+            if (this.props.className) {
+                classesSet.push(this.props.className);
+            }
 
-      classesSet.push(block);
+            classesSet.push(block);
 
-      const
-        modifiersFromProps = (modifiers || [])
-          .filter(modifierName=>!!this.props[modifierName])
-          .map(modifierName => createModifier(block, modifierName, this.props[modifierName])),
-        modifiersFromArguments = modifiersFromObj(block, passedmodifiers);
+            const
+                modifiersFromProps = (modifiers || [])
+                    .filter(modifierName => !!this.props[modifierName])
+                    .map(modifierName => createModifier(block, modifierName, this.props[modifierName])),
+                modifiersFromArguments = modifiersFromObj(block, passedmodifiers);
 
-      classesSet.push(...modifiersFromProps, ...modifiersFromArguments);
+            classesSet.push(...modifiersFromProps, ...modifiersFromArguments);
 
-      return classesSet.join(' ');
-    },
+            return classesSet.join(' ');
+        },
 
-    element(elementName, modifiers){
-      const
-        elementClass = `${block}__${elementName}`,
-        modifiersClasses = modifiersFromObj(elementClass, modifiers);
+        element(elementName, modifiers) {
+            const
+                elementClass = `${block}__${elementName}`,
+                modifiersClasses = modifiersFromObj(elementClass, modifiers);
 
-      return [
-        elementClass,
-        ...modifiersClasses
-      ].join(' ');
-    }
-  });
+            return [
+                elementClass,
+                ...modifiersClasses
+            ].join(' ');
+        }
+    });
 
-  return Component;
+    return Component;
 
 };
